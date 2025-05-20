aa9f36c82656e04f83dde5a69eb51604f8414fa8
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-05-20 15:30:10 +0200

    Uppdatering för spel knappar
    Jag har lagt in vad Sebastian har skrivit i index.js brach js, kommer behöva fixas ännu mer

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ function createProfilePage() {
             <button class="buttons" id="play">PLAY</button>
         </div>
     `;
+    //Play button har id="startButton" från sebbes sida
 
     footerDOM.innerHTML = `
     <div id="info">
@@ -129,8 +130,107 @@ function createProfilePage() {
     bodyDOM.appendChild(footerDOM);
 }
 
+/*Från Sebastians js branch index.js (20/5 15:23) */
 
-/*----------------------------------------------------------------------------------------------------------------- */
+let selectedDifficulty = null;
+let selectedAnimal = null;
+
+const difficultyButtons = document.querySelectorAll(".difficultyButton");
+for (let i = 0; i < difficultyButtons.length; i++) {
+    difficultyButtons[i].addEventListener("click", function () {
+        selectedDifficulty = this.value;
+
+        for (let j = 0; j < difficultyButtons.length; j++) {
+            difficultyButtons[j].classList.remove("selected");
+        }
+        this.classList.add("selected")
+    })
+}
+
+const animalButtons = document.querySelectorAll(".animalButton");
+for (let i = 0; i < animalButtons.length; i++) {
+    animalButtons[i].addEventListener("click", function () {
+        selectedAnimal = this.value;
+
+        for (let j = 0; j < animalButtons.length; j++) {
+            animalButtons[j].classList.remove("selected")
+        }
+        this.classList.add("selected")
+    })
+}
+//Vi skapar två varibler. En för svårighetsgrad och en för djuret.
+//Vi loopar igenom alla knappar och tar ut värden från knappen som klickas.
+//Använder en nästlad loop för att ta bort classen selected från en knapp.
+//Sen lägger vi även till classen selected för den man trycker på.
+
+let playButtonDOM = document.querySelector("#playButton");
+playButtonDOM.addEventListener("click", async function () {
+
+    const numberOfCards = Number(selectedDifficulty);
+    const animalValue = selectedAnimal;
+    const numberOfUniqueImages = numberOfCards / 2;
+
+    let images = [];
+
+    async function getImage(animal) {
+        if (animal === "dog") {
+            const response = await fetch("https://dog.ceo/api/breeds/image/random");
+            const data = await response.json();
+            return data.message;
+        }
+        if (animal === "fox") {
+            const response = await fetch("https://randomfox.ca/floof/");
+            const data = await response.json();
+            return data.image;
+        }
+        if (animal === "cat") {
+            const response = await fetch("https://api.thecatapi.com/v1/images/search");
+            const data = await response.json();
+            return data[0].url;
+        }
+    }
+
+    for (let i = 0; i < numberOfUniqueImages; i++) {
+        const imageURL = await getImage(animalValue);
+        images.push(imageURL);
+    }
+    //Här loopar vi igenom hälften så många gånger som svårighetsgraden. Till exempel
+    //Svårighetsgrad lätt vilket är 6 kort i memoryt, då loopar vi igenom 3 gånger.
+    //Vi anropar då getImage 3 gånger där vi fetchar det valda djuret.
+    //Vi får ut 3 bilder som vi pushar till image arrayen.
+
+    let cards = [];
+    for (let i = 0; i < images.length; i++) {
+        cards.push(images[i]);
+        cards.push(images[i]);
+    }
+    //Vi skapar en cards array. Vi loopar igenom images där vi nu har i detta fall 3 bilder.
+    //För varje instans så pushar vi det objektet i arrayen två gånger.
+    //cards inehåller nu 6 objekt.
+
+    cards.sort(function () {
+        return Math.random() - 0.5;
+    })
+    //Här blandar vi korten. Förstår inte riktigt denna rad men ska se om det finns ett 
+    //enklare sätt att göra det.
+
+    const gameBoard = document.querySelector("#gameBoard");
+    gameBoard.innerHTML = "";
+
+    for (let i = 0; i < cards.length; i++) {
+        const img = document.createElement("img");
+        img.src = cards[i];
+        img.style.width = "100px"
+        gameBoard.appendChild(img);
+    }
+    //Här selekterar vi vart vi vill att korten ska hamna.
+    //Vi loopar sedan igenom cards och skapar en img för varje instans.
+})
+/*SLUT p åfrån Sebastians kod från branch js index.js (20/5 15:23) */
+/*------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*Från test sidan! Från denna rad till rad 232! */
 async function GETCurrentUser() {
     const response = await fetch("http://localhost:8000/profil", {
