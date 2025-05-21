c5b78f5f868adf0c741068a123a0787ac534ce53
JonahBolin <jonah.bolin17@gmail.com>
2025-05-21 11:14:04 +0200

    Fixade oändlig-loop-bugg
    Det blev en oändlig loop, alltså det skapades oändligt många arrayer med oändligt många bilder i varje pga for-loopen. Gjorde en forEach-loop istället, vilket löste problemet.

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,7 @@ startButton.addEventListener("click", async function () {
         if (animal === "dog") {
             const response = await fetch("https://dog.ceo/api/breeds/image/random");
             const data = await response.json();
+            console.log(data.message);
             return data.message;
         }
         if (animal === "fox") {
@@ -68,40 +69,43 @@ startButton.addEventListener("click", async function () {
 
     for (let i = 0; i < numberOfUniqueImages; i++) {
         const imageURL = await getImage(animalValue);
+        console.log(imageURL);
         const id = i;
         console.log(id, "id för bilden");
         const image = new Card(imageURL, id, animalValue);
         images.push(image);
     }
+
+    console.log(images.length, "den nuvarande längden på arrayen, borde vara 3");
     //Här loopar vi igenom hälften så många gånger som svårighetsgraden. Till exempel
     //Svårighetsgrad lätt vilket är 6 kort i memoryt, då loopar vi igenom 3 gånger.
     //Vi anropar då getImage 3 gånger där vi fetchar det valda djuret.
     //Vi får ut 3 bilder som vi pushar till image arrayen.
-    for (let i = 0; i < images.length; i++) {
-        const originalImage = images[i];
-        const copyOfOrginialImage = new Card(originalImage.id, originalImage.id, originalImage.theme);
-        images.push(copyOfOrginialImage);
-        console.log(images, images.length, "bör innehålla alla korten, alltså alla par");
-    }
-    //Vi skapar en cards array. Vi loopar igenom images där vi nu har i detta fall 3 bilder.
-    //För varje instans så pushar vi det objektet i arrayen två gånger.
-    //cards inehåller nu 6 objekt.
+    // images.forEach(img => {
+    //     const copyOfOrginialImage = new Card(img.url, img.id, img.theme);
+    //     images.push(copyOfOrginialImage);
+    //     console.log(images, images.length, "bör innehålla alla korten, alltså alla par");
+    // })
 
-    images.sort(function () {
-        return Math.random() - 0.5;
-    })
-    //Här blandar vi korten. Förstår inte riktigt denna rad men ska se om det finns ett 
-    //enklare sätt att göra det.
+    // //Vi skapar en cards array. Vi loopar igenom images där vi nu har i detta fall 3 bilder.
+    // //För varje instans så pushar vi det objektet i arrayen två gånger.
+    // //cards inehåller nu 6 objekt.
 
-    const gameBoard = document.querySelector("#gameBoard");
-    gameBoard.innerHTML = "";
+    // images.sort(function () {
+    //     return Math.random() - 0.5;
+    // })
+    // //Här blandar vi korten. Förstår inte riktigt denna rad men ska se om det finns ett 
+    // //enklare sätt att göra det.
 
-    for (let i = 0; i < images.length; i++) {
-        const img = document.createElement("img");
-        img.src = images[i];
-        img.style.width = "100px"
-        gameBoard.appendChild(img);
-    }
+    // const gameBoard = document.querySelector("#gameBoard");
+    // gameBoard.innerHTML = "";
+
+    // for (let i = 0; i < images.length; i++) {
+    //     const img = document.createElement("img");
+    //     img.src = images[i];
+    //     img.style.width = "100px"
+    //     gameBoard.appendChild(img);
+    // }
     //Här selekterar vi vart vi vill att korten ska hamna.
     //Vi loopar sedan igenom cards och skapar en img för varje instans.
 })
