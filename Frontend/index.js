fdf654f463fd184bd2dd31790b1bc8e0bda82e00
Sebastianrosberg <sebbe.rosberg@gmail.com>
2025-05-28 15:29:03 +0200

    jaja

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,5 @@
 class Card {
-    constructor(url, theme) {
+    constructor(url, id, theme) {
         this.url = url;
         this.theme = theme;
         this.flipped = false;
@@ -389,14 +389,10 @@ async function playGame(selectedDifficulty, selectedTheme, selectedPoints) {
                         firstCard.cardData.flipStatusTrue();
                         secondCard.cardData.flipStatusTrue();
 
-                        firstCard.style.transform = "rotateY(180deg)"
-                        secondCard.style.transform = "rotateY(180deg)"
                     } else {
                         firstCard.cardData.flipStatusFalse();
                         secondCard.cardData.flipStatusFalse();
 
-                        firstCard.style.transform = "rotateY(180deg)"
-                        secondCard.style.transform = "rotateY(180deg)"
                         firstCard.style.backgroundImage = "url(`img/backside.png`)";
                         secondCard.style.backgroundImage = "url(`img/backside.png`)";
 
@@ -505,7 +501,7 @@ async function POSTHandlerRegistration(username, password, password2) {
     });
 
     if (response.status === 200) {
-
+        alert("User registered successfully! Please log in.");
         login()
     } else if (response.status === 409) {
         alert("1. Användaren finns redan!");
@@ -525,6 +521,7 @@ async function GETLogin(username, password) {
     // document.body.appendChild(message);
 
     if (response.status === 200) {
+        alert("Login was successful!");
         // message.textContent = "2. Inloggning genomförd!";
         await GETCurrentUser();
         homePage();
@@ -579,6 +576,7 @@ async function PATCHScore(username, score) {
     document.body.appendChild(message);
 
     if (response.status === 200) {
+        // alert("4. Poäng har adderats till totalpoängen för användaren!");
         console.log("Poängen har uppdaterats för användaren");
 
         // message.textContent = "4. Poäng har adderats till totalpoängen för användaren!";
@@ -608,7 +606,7 @@ async function PATCHExitGame(username) {
     document.body.appendChild(message);
 
     if (response.status === 200) {
-
+        alert("Spel avbrutet, poängen uppdaterades ej.");
         // message.textContent = "5. Spel avbrutet, poängen uppdaterades ej.";
     } else if (response.status === 404) {
         alert("Något gick fel vid avslut.");
